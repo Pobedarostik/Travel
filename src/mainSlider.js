487979f1a67f2e0167b8270b5a0da76f8e4735ee
Pobedarostik <pobedarostik@gmail.com>
2022-10-06 18:44:39 +0300

    done all sections

--- src/mainSlider.js
+++ src/mainSlider.js
@@ -1,8 +1,7 @@
-const slider = document.querySelector('.popularExcursions_slaider');
 const sliderLine = document.querySelector('.popularExcursions_slaider_line');
-const btnLeft = document.querySelector('.btnScrolling_left');
-const btnRigth = document.querySelector('.btnScrolling_right');
-const lineProgres = document.querySelector('.scrolling_progres_secces');
+const btnLeft = document.querySelector('.popularExcursions_btnScrolling_left');
+const btnRigth = document.querySelector('.popularExcursions_btnScrolling_right');
+const lineProgres = document.querySelector('.popularExcursions_scrolling_progres_secces');
 
 let offset = 0;
 let progres = 0;
@@ -28,4 +27,45 @@ btnLeft.addEventListener('click', () => {
 
   sliderLine.style.left = -offset + 'px';
   lineProgres.style.left = progres + 'px';
+});
+
+// -----------Slider to Rewiews--------------//
+const Rewiews_slider = document.querySelector('.reviews_slaider');
+const Rewiews_sliderLine = document.querySelector('.reviews_slaider_line');
+const Rewiews_btnLeft = document.querySelector('.reviews_btnScrolling_left');
+const Rewiews_btnRigth = document.querySelector('.reviews_btnScrolling_right');
+const Rewiews_lineProgres = document.querySelector('.reviews_scrolling_progres_secces');
+
+let Rewiews_offset = 0;
+let Rewiews_progres = 0;
+
+// Rewiews_btnRigth.addEventListener('click', offsetRight);
+
+// function offsetRight() {
+//   Rewiews_offset += 350;
+//   Rewiews_progres += 350;
+//   Rewiews_sliderLine.style.left = -Rewiews_offset + 'px';
+//   Rewiews_lineProgres.style.left = Rewiews_progres + 'px';
+// }
+Rewiews_btnRigth.addEventListener('click', () => {
+  Rewiews_offset += 760;
+  Rewiews_progres += 258.5;
+  if (Rewiews_offset > 2300) {
+    Rewiews_offset = 0;
+    Rewiews_progres = 0;
+  }
+  console.log(Rewiews_progres);
+  Rewiews_sliderLine.style.left = -Rewiews_offset + 'px';
+  Rewiews_lineProgres.style.left = Rewiews_progres + 'px';
+});
+Rewiews_btnLeft.addEventListener('click', () => {
+  Rewiews_offset -= 760;
+  Rewiews_progres -= 258.5;
+  if (Rewiews_offset < 0) {
+    Rewiews_offset = 2300;
+    Rewiews_progres = 775.5;
+  }
+
+  Rewiews_sliderLine.style.left = -Rewiews_offset + 'px';
+  Rewiews_lineProgres.style.left = Rewiews_progres + 'px';
 });
