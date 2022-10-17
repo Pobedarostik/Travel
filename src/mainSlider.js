651ddc6d1f7b462a7012717c1be0665897bb76a1
Pobedarostik <pobedarostik@gmail.com>
2022-10-17 19:22:47 +0300

    done adapt excursions

--- src/mainSlider.js
+++ src/mainSlider.js
@@ -71,10 +71,3 @@ Rewiews_btnLeft.addEventListener('click', () => {
 });
 
 // menu Burger
-const burger = document.querySelector('.menu_burger');
-const header_nav = document.querySelector('.header_nav');
-
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active');
-  header_nav.classList.toggle('active');
-});
